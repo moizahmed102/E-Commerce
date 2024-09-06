d60f24455dc1f0ae04f00ac68b80c0231c91953f
moizahmed102 <moizahmed1825@gmail.com>
2024-09-06 18:03:09 +0500

    Updated routes of cart/order/product

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createOrder, getUserOrders } from "../controllers/orderController.js";
+import { createOrder, getOrders } from "../controllers/orderController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post("/", createOrder);
-router.get("/", getUserOrders);
+router.post("/checkout", createOrder);
+router.get("/all", getOrders);
 
 export default router;
