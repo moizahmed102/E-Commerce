0ff8a30da488ad3e95dd942c13cd848ba898fcb1
moizahmed102 <moizahmed1825@gmail.com>
2024-09-06 18:01:53 +0500

    Added size field

--- backend/model/Product.js
+++ backend/model/Product.js
@@ -14,6 +14,10 @@ const productSchema = new mongoose.Schema({
     ref: "Category",
     required: true,
   },
+  size: {
+    type: [String],
+    default: ["S", "M", "L"],
+  },
   image: {
     type: String,
     required: true,
