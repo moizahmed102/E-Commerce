703914232d6e26a6093c4c1861dae343b2428c2c
moizahmed102 <moizahmed1825@gmail.com>
2024-09-16 12:34:20 +0500

    redirecting unauth users

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import SignupForm from "../components/Signup"
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 function Signup() {
   return (
-   
+   <>
+   <Header />
     <SignupForm/>
-   
+    <Footer />
+    </>
   )
 }
 
